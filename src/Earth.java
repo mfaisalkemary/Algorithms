9fea1e1216c83817be7b10af6d2691759042eede
mfaisal <mfaisalkemary@gmail.com>
2018-01-02 18:32:19 +0300

    Initial commit

---
+++ src/Earth.java
@@ -0,0 +1,16 @@
+public class Earth {
+
+public static void main(String [] args){
+    Human human1;
+    Human human2;
+    Human human3;
+
+    human1 = new Human("mohamed",22,185,"Brown");
+    human2 =new Human("hassan",28,189,"Blue");
+
+    human1.Speak();
+    human2.Speak();
+
+
+}
+}
