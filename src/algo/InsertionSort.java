18404621f4f27df069246245be1ae7c55c0f5e66
mohamed faisal <mfaisalkemary@gmail.com>
2018-11-21 18:51:48 +0300

    adding insertion sort

--- src/algo/InsertionSort.java
+++ src/algo/InsertionSort.java
@@ -5,20 +5,22 @@ import java.util.Arrays;
 
 public class InsertionSort extends Algorithm{
 
-    public static int[] sort(int[]array){
+    public static int[] sortWrong(int[]array){
         try {
+            int [] array1;
             int steps=0;
             for (int i=1;i<array.length;i++){
                 steps++;
                 int temp=0;
                 int min = array[i];
-                for (int j=i-1;j>=0;j--){
+                int t =i;
+                for (int j=t-1;j>=0;j--){
                     steps++;
                     if(min<array[j]){
                         temp=array[j];
-                        array[i]=temp;
+                        array[t]=temp;
                         array[j]=min;
-                        i--;
+                        t--;
                     }
 
 
@@ -26,7 +28,32 @@ public class InsertionSort extends Algorithm{
 
             }
 
-            System.out.println("Number of Steps is : "+steps);
+            System.out.println("Number of Steps is : "+steps+"\n");
+            array1=array;
+            return array1;
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+    public static int[] sortByInstructor(int[]array){
+        try {
+
+            for (int i=1;i<array.length;i++){
+                int element=array[i];
+                int j =i-1;
+                while (j>=0 && array[j]>element){
+                    array[j+1]=array[j];
+                    j--;
+                }
+                array[j+1]=element;
+
+            }
+
             return array;
 
         }
@@ -37,11 +64,48 @@ public class InsertionSort extends Algorithm{
 
     }
 
+    public static int[] insertionSort(int [] array){
+        try {
+            int steps =0;
+
+            for (int i=1;i<array.length;i++){
+                steps++;
+                int element = array[i];
+                for (int j=i-1;j>=0;j--){
+                    steps++;
+                    if (array[j]>element){
+                        //element=array[j];
+                        array[j+1]=array[j];
+                        array[j]=element;
+                        element=array[j];
+                    }
+
+
+                }
+
+            }
+            System.out.println(steps);
+            return array;
+
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
 
     public static void main(String[] args) {
         int[]array =createIntArray(10);
-        System.out.println("Sorted By Me  "+Arrays.toString(sort(array)));
-        sortArrayByJava(array);
+
+        //System.out.println("Sorted By Me  "+Arrays.toString(sort(array)));
+
+      //  System.out.println("Sorted By Instructor  "+Arrays.toString(sortByInstructor(array)));
+
+       // sortArrayByJava(array);
+        System.out.println("Sorted By me  "+Arrays.toString(insertionSort(array)));
+
 
     }
 }
